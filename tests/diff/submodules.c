ffd199d91486b314c9cc99c5d5ee81d82b43150c
Josh Vera <vera@github.com>
2014-09-26 20:12:54 -0400

    dont free prematurely

--- tests/diff/submodules.c
+++ tests/diff/submodules.c
@@ -504,17 +504,18 @@ void test_diff_submodules__can_commit_subrepo(void) {
 		git_commit *commit;
 
 		cl_git_pass(git_repository_init(&r2, "empty_standard_repo/subrepo", 0));
-		git_repository_free(r2);
 
 		cl_git_mkfile("empty_standard_repo/subrepo/README.txt", "hello\n");
 
 		cl_git_pass(git_repository_index(&r2index, r2));
-		cl_git_pass(git_index_add_bypath(r2index, "empty_standard_repo/subrepo/README.txt"));
+		cl_git_pass(git_index_add_bypath(r2index, "README.txt"));
 		cl_repo_commit_from_index(&oid, r2, NULL, 0, "first commit");
 
 		cl_git_pass(git_commit_lookup(&commit, r2, &oid));
 
 		cl_assert_equal_i(0, git_commit_parentcount(commit));
+
+		git_repository_free(r2);
 	}
 
 	git_index *index;
